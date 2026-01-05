d1037dc203e3058f04bdb448b6936e3b30e30be6
Ameth Toledo <233363@ids.upchiapas.edu.mx>
2026-01-04 19:25:20 -0600

    ci: make eslint rules less strict (warnings instead of errors)

--- eslint.config.js
+++ eslint.config.js
@@ -1,44 +1,41 @@
 // @ts-check
 const eslint = require("@eslint/js");
-const { defineConfig } = require("eslint/config");
 const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
 
-module.exports = defineConfig([
+module.exports = tseslint.config(
   {
     files: ["**/*.ts"],
     extends: [
       eslint.configs.recommended,
-      tseslint.configs.recommended,
-      tseslint.configs.stylistic,
-      angular.configs.tsRecommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
     ],
     processor: angular.processInlineTemplates,
     rules: {
-      "@angular-eslint/directive-selector": [
-        "error",
-        {
-          type: "attribute",
-          prefix: "app",
-          style: "camelCase",
-        },
-      ],
-      "@angular-eslint/component-selector": [
-        "error",
-        {
-          type: "element",
-          prefix: "app",
-          style: "kebab-case",
-        },
-      ],
+      "@typescript-eslint/no-empty-function": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@angular-eslint/prefer-inject": "warn",
+      "@angular-eslint/no-empty-lifecycle-method": "warn",
+      "prefer-const": "warn",
     },
   },
   {
     files: ["**/*.html"],
     extends: [
-      angular.configs.templateRecommended,
-      angular.configs.templateAccessibility,
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
     ],
-    rules: {},
+    rules: {
+      "@angular-eslint/template/click-events-have-key-events": "warn",
+      "@angular-eslint/template/interactive-supports-focus": "warn",
+      "@angular-eslint/template/label-has-associated-control": "warn",
+      "@angular-eslint/template/prefer-control-flow": "warn",
+      "@angular-eslint/template/no-autofocus": "warn",
+      "@angular-eslint/no-output-on-prefix": "warn",
+      "@angular-eslint/use-lifecycle-interface": "warn",
+    },
   }
-]);
+);
